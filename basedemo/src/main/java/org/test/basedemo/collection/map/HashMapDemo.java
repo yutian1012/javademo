c5c1365fb384b61b0e8ca6ecf48dd6a622a00d47
yutian1012 <yutian1012@126.com>
2018-08-12 23:31:32 +0800

    u

---
+++ basedemo/src/main/java/org/test/basedemo/collection/map/HashMapDemo.java
@@ -0,0 +1,11 @@
+package org.test.basedemo.collection.map;
+
+import java.util.HashMap;
+
+public class HashMapDemo {
+	public static void main(String[] args) {
+		new HashMap<>();
+		
+		ClassLoader loader=null;
+	}
+}
